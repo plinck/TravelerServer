3ee3223e17eadc5bb0deec57491c0753ffde4418
plinck <paul.linck@gmail.com>
2019-01-20 11:45:39 -0500

    added routes

--- index.js
+++ index.js
@@ -12,10 +12,11 @@
 "use strict";
 
 const http = require("http");
+const url = require("url");
 const fs = require("fs");
 const weatherDB = require("./weatherModule.js");
 const favDB = require("./favoritesModel.js");
-const googDB = require("./googleAPIModel.js");
+const googDB = require("./googleAPIModule.js");
 
 const hostname = "localhost";
 const port = 3000;
@@ -24,33 +25,41 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/JSON");
 
-    // weatherDB.getWeatherDB({
-    //     lat: 33,
-    //     lng: -84
-    // }, (currentWeather, dailyWeather) => {
-    //     console.log(currentWeather);
-    //     console.log(dailyWeather);
-    //     res.write(JSON.stringify(currentWeather));
-    //     res.write(JSON.stringify(dailyWeather));
-    //     res.end();
-    // }, (errCode) => {
+    let path = url.parse(req.url).pathname;
+    switch (path) {
+        case "/":
+            console.log(path);
+            break;
+        case "/weather":
+            console.log(path);
+            weatherDB.getWeatherDB({
+                lat: 33,
+                lng: -84
+            }, (currentWeather, dailyWeather) => {
+                console.log(currentWeather);
+                console.log(dailyWeather);
+                res.write(JSON.stringify(currentWeather));
+                res.write(JSON.stringify(dailyWeather));
+                res.end();
+            }, (errCode) => {});
+            break;
+        case "/locationsByName":
+            console.log(path);
+            favDB.favoritesGetByName((favsDB) => {
+                let favoritePlaces = favsDB; // copy the array into the global var for this context
 
-    // });
-
-    favDB.favoritesGetByName((favsDB) => {
-        let favoritePlaces = favsDB; // copy the array into the global var for this context
-
-        googDB.getLatLongForPlace(favoritePlaces[0], (newDataForAddress) => {
-            console.log(newDataForAddress);
-            favoritePlaces[0].lat = newDataForAddress.lat;
-            favoritePlaces[0].lng = newDataForAddress.lng;
-            res.write(JSON.stringify(favoritePlaces));
-            res.end();
-        }, (errCode) => {
-            console.log(errCode);
-        });
-    });
-
+                googDB.getLatLongForPlace(favoritePlaces[0], (newDataForAddress) => {
+                    console.log(newDataForAddress);
+                    favoritePlaces[0].lat = newDataForAddress.lat;
+                    favoritePlaces[0].lng = newDataForAddress.lng;
+                    res.write(JSON.stringify(favoritePlaces));
+                    res.end();
+                }, (errCode) => {
+                    console.log(errCode);
+                });
+            });
+            break;
+    }
 });
 
 server.listen(port, hostname, () => {
